36ac2b508fe1d048e16de12298a5f453405d43d2
ger-tri <gerardtrinio@gmail.com>
2023-07-13 01:42:58 +0100

    Continuing to style and add functionality: + added dialog tooltips + reformatted sidebar + added character summaries to sidebar     + fixed 'go back' functionality (no infinite loop on characters)     + character summaries is disabled until the user reads them all + added possible emphasis colours for text
    - removed default sidebar buttons

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -13,3 +13,27 @@ $(document).on(":passagerender", function (event) {
 });
 
 Config.ui.stowBarInitially = true;
+
+/* see https://www.motoslave.net/sugarcube/2/docs/#guide-tips-arbitrarily-long-return
+   use $return to go back to a non-noreturn passage
+e.g. <<link "GO BACK" $return>><</link>> */
+$(document).on(':passagestart', function (ev) {
+	if (!ev.passage.tags.includes('noreturn')) {
+		State.variables.return = ev.passage.title;
+	}
+});
+
+// https://stackoverflow.com/questions/2847561/add-a-different-id-to-each-li-element-by-jquery
+// Execute the handler function each time the event triggers.
+$(document).on(':passageend', function (ev) { // this has to be done after the special passage 'StoryMenu' runs
+	if (State.variables.isUnlockCharacterSummaries == true) {
+		// do nothing; will automatically unlock
+	} else {
+		$('#menu-story li a').attr('id', function(i) {
+			return 'menubutton'+(i+1);
+		});
+		document.getElementById("menubutton1").setAttribute("aria-disabled",true);
+		document.getElementById("menubutton1").setAttribute("disabled","");
+		document.getElementById("menubutton1").setAttribute("type","button");
+	}
+});
